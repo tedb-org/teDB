5c9700e79dc4502a68584e37e924552a857560a3
Travis Sturzl <travissturzl@gmail.com>
2017-04-19 16:24:15 -0600

    create index

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,14 @@
  * Created by tsturzl on 4/11/17.
  */
 
+import Datastore from "./datastore";
+import Cursor from "./cursor";
+import Index from "./indices";
+import { IStorageDriver } from "./types/storageDriver";
+import { IRange } from "./types/range";
+
+export { Datastore };
+export { Cursor };
+export { Index };
+export { IStorageDriver };
+export { IRange };
